647feda5af45f89ba1a9598a300a813b64caf83c
xl <1143393767@qq.com>
2021-05-18 10:45:50 +0800

    java进阶

--- kafka/src/test/java/com/xl/KafkaApplicationTest.java
+++ kafka/src/test/java/com/xl/KafkaApplicationTest.java
@@ -1,28 +1,17 @@
 package com.xl;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
-@RunWith(SpringRunner.class)//一定要带，不然得不到很多bean
 public class KafkaApplicationTest {
 
-    @Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Test
     public void contextLoads() {
 
-    }
-
-    @Test
-    public void test1() {
-        kafkaTemplate.send("topic20" , "hello");
     }
 
 
